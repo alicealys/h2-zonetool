c2002804b449d41d6875b91158e1feccde7d9740
fed <58637860+fedddddd@users.noreply.github.com>
2022-11-04 22:10:22 +0100

    Fix xmodel

--- src/client/zonetool/structs.hpp
+++ src/client/zonetool/structs.hpp
@@ -1152,6 +1152,175 @@ namespace zonetool
 
 	static_assert(sizeof(AddonMapEnts) == 136);
 
+	struct pathnode_yaworient_t
+	{
+		float fLocalAngle;
+		float localForward[2];
+	};
+
+	union $3936EE84564F75EDA6DCBAC77A545FC8
+	{
+		pathnode_yaworient_t yaw_orient;
+		float angles[3];
+	};
+
+	union PathNodeParentUnion
+	{
+		scr_string_t name;
+		unsigned short index;
+	};
+
+	enum PathNodeErrorCode : std::int32_t
+	{
+		PNERR_NONE = 0x0,
+		PNERR_INSOLID = 0x1,
+		PNERR_FLOATING = 0x2,
+		PNERR_NOLINK = 0x3,
+		PNERR_DUPLICATE = 0x4,
+		PNERR_NOSTANCE = 0x5,
+		PNERR_INVALIDDOOR = 0x6,
+		PNERR_NOANGLES = 0x7,
+		PNERR_BADPLACEMENT = 0x8,
+		NUM_PATH_NODE_ERRORS = 0x9,
+	};
+
+	union $5F11B9753862CE791E23553F99FA1738
+	{
+		float minUseDistSq;
+		PathNodeErrorCode error;
+	};
+
+	struct pathnode_t;
+
+	struct pathlink_s
+	{
+		float fDist;
+		unsigned short nodeNum;
+		unsigned char disconnectCount;
+		unsigned char negotiationLink;
+		unsigned char flags;
+		unsigned char ubBadPlaceCount[3];
+	};
+
+	struct pathnode_constant_t
+	{
+		unsigned short type;
+		unsigned int spawnflags;
+		scr_string_t targetname;
+		scr_string_t script_linkName;
+		scr_string_t script_noteworthy;
+		scr_string_t target;
+		scr_string_t animscript;
+		int animscriptfunc;
+		float vLocalOrigin[3];
+		$3936EE84564F75EDA6DCBAC77A545FC8 ___u9;
+		PathNodeParentUnion parent;
+		$5F11B9753862CE791E23553F99FA1738 ___u11;
+		short wOverlapNode[2];
+		char __pad0[4];
+		unsigned short totalLinkCount;
+		pathlink_s* Links;
+		scr_string_t unk;
+		char __pad1[4];
+	};
+
+	struct SentientHandle
+	{
+		unsigned short number;
+		unsigned short infoIndex;
+	};
+
+	struct pathnode_dynamic_t
+	{
+		SentientHandle pOwner;
+		int iFreeTime;
+		int iValidTime[3];
+		short wLinkCount;
+		short wOverlapCount;
+		short turretEntNumber;
+		unsigned char userCount;
+		unsigned char hasBadPlaceLink;
+		int spreadUsedTime[2];
+		short flags;
+		short dangerousCount;
+		int recentUseProxTime;
+	};
+
+	union $73F238679C0419BE2C31C6559E8604FC
+	{
+		float nodeCost;
+		int linkIndex;
+	};
+
+	struct pathnode_transient_t
+	{
+		int iSearchFrame;
+		pathnode_t* pNextOpen;
+		pathnode_t* pPrevOpen;
+		pathnode_t* pParent;
+		float fCost;
+		float fHeuristic;
+		$73F238679C0419BE2C31C6559E8604FC ___u6;
+	};
+
+	struct pathnode_t
+	{
+		pathnode_constant_t constant;
+		pathnode_dynamic_t dynamic;
+		pathnode_transient_t transient;
+	};
+
+	struct pathnode_tree_nodes_t
+	{
+		int nodeCount;
+		unsigned short* nodes;
+	};
+
+	struct pathnode_tree_t;
+	union pathnode_tree_info_t
+	{
+		pathnode_tree_t* child[2];
+		pathnode_tree_nodes_t s;
+	};
+
+	struct pathnode_tree_t
+	{
+		int axis;
+		float dist;
+		pathnode_tree_info_t u;
+	};
+
+	struct PathDynamicNodeGroup
+	{
+		unsigned short parentIndex;
+		int nodeTreeCount;
+		pathnode_tree_t* nodeTree;
+	};
+
+	struct PathData
+	{
+		const char* name;
+		unsigned int nodeCount;
+		pathnode_t* nodes;
+		bool parentIndexResolved;
+		unsigned short version;
+		int visBytes;
+		unsigned char* pathVis;
+		int nodeTreeCount;
+		pathnode_tree_t* nodeTree;
+		int dynamicNodeGroupCount;
+		PathDynamicNodeGroup* dynamicNodeGroups;
+		int exposureBytes;
+		unsigned char* pathExposure;
+		int noPeekVisBytes;
+		unsigned char* pathNoPeekVis;
+		int zoneCount;
+		int zonesBytes;
+		unsigned char* pathZones;
+		int dynStatesBytes;
+		unsigned char* pathDynStates;
+	}; assert_sizeof(PathData, 0x88);
+
 	struct RawFile
 	{
 		const char* name;
@@ -2214,7 +2383,8 @@ namespace zonetool
 		unsigned char numBonePhysics; // 12
 		unsigned char numCompositeModels; // 13
 		char __pad0[2]; // 14-16
-		float scale; // 16
+		float unk_float; // 16
+		float scale;
 		unsigned int noScalePartBits[8]; // 20
 		scr_string_t* boneNames; // 56
 		unsigned char* parentList; // 64
@@ -2251,6 +2421,11 @@ namespace zonetool
 		XModel** compositeModels; // 680
 		XPhysBoneInfo* bonePhysics; // 688
 	};  static_assert(sizeof(XModel) == 0x2B8);
+
+	assert_offsetof(XModel, noScalePartBits, 24);
+	assert_offsetof(XModel, boneNames, 56);
+	assert_offsetof(XModel, bonePhysics, 688);
+	assert_offsetof(XModel, lodInfo, 120);
 
 	enum activeReticleType_t : std::int32_t
 	{
